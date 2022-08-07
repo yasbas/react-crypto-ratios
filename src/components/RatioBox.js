498311221258deacb2b103e249db80a291d1b906
Yasen Georgiev <yasbas@gmail.com>
2022-08-07 16:57:16 +0300

    Small fixes.

--- src/components/RatioBox.js
+++ src/components/RatioBox.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 const RatioBox = ({base, quote}) => {
     const onBtnGet = () => {
-        const getData = fetch('https://www.binance.com/api/v1/ticker/allPrices')
+        fetch('https://www.binance.com/api/v1/ticker/allPrices')
         .then(response => response.json())
         .then(json => json.filter(pair =>
-            pair.symbol == base+'BUSD' ||
-            pair.symbol == quote+'BUSD'
+            pair.symbol === base+'BUSD' ||
+            pair.symbol === quote+'BUSD'
         ))
         .then(basequote => {
             console.log(basequote)
-            const quoteVal = basequote.filter(pair => pair.symbol == quote+'BUSD')[0]
-            const baseVal = basequote.filter(pair => pair.symbol == base+'BUSD')[0]
+            const quoteVal = basequote.filter(pair => pair.symbol === quote+'BUSD')[0]
+            const baseVal = basequote.filter(pair => pair.symbol === base+'BUSD')[0]
             const ratio = (parseFloat(baseVal.price) / parseFloat(quoteVal.price)).toFixed(4)
             const ratioDisplay = document.getElementById('ratioDisplay'+base+quote)
 
