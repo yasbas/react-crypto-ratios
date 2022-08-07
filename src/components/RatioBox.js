275e1863b711cccad64e368c2416f41fb34007a6
Yasen Georgiev <yasbas@gmail.com>
2022-08-07 15:20:27 +0300

    Cleanup

--- src/components/RatioBox.js
+++ src/components/RatioBox.js
@@ -1,24 +1,10 @@
 import React from 'react'
 
 const RatioBox = ({base, quote}) => {
-    // const getData = fetch('https://jsonplaceholder.typicode.com/todos/')
-    //     .then(response => response.json())
-    //     //.then(json => console.log(json));
-
     const onBtnGet = () => {
-        //console.log(getData.getResult()[0])
-        //const getData = fetch('https://jsonplaceholder.typicode.com/todos/')
         const getData = fetch('https://www.binance.com/api/v1/ticker/allPrices')
         .then(response => response.json())
-        //.then(json => console.log(json))
-        .then(json => json.filter(pair => 
-            // pair.symbol.endsWith('USDТ') && 
-            // !pair.symbol.includes('UP') &&
-            // !pair.symbol.includes('DOWN') &&
-            // !pair.symbol.includes('BULL') &&
-            // !pair.symbol.includes('BEAR')
-            // pair.symbol == 'MATICBUSD' ||
-            // pair.symbol == 'XRPBUSD'
+        .then(json => json.filter(pair =>
             pair.symbol == base+'BUSD' ||
             pair.symbol == quote+'BUSD'
         ))
@@ -28,7 +14,7 @@ const RatioBox = ({base, quote}) => {
             const baseVal = basequote.filter(pair => pair.symbol == base+'BUSD')[0]
             const ratio = (parseFloat(baseVal.price) / parseFloat(quoteVal.price)).toFixed(4)
             const ratioDisplay = document.getElementById('ratioDisplay'+base+quote)
-            
+
             ratioDisplay.innerText = ratio
 
             const prices = document.getElementById('prices'+base+quote)
