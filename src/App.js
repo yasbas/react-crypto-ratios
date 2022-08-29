8402db001893a5b6a31819c3e4119ff462934612
Yas <yasbas@gmail.com>
2022-08-29 23:21:44 +0300

    Forgot the httpS.

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
 		// Load positions
 		// CORS issues: https://developer.okta.com/blog/2021/08/02/fix-common-problems-cors#how-to-solve-a-simple-cors-issue
 		// fetch('http://localhost:8001/api/json', {referrerPolicy: 'unsafe-url'})
-		fetch('http://yasbas.com/cryptobe/web/api/json')
+		fetch('https://yasbas.com/cryptobe/web/api/json')
 			.then(response => response.json())
 			.then(json => {
 				// Return the positions, filtering the test positions
